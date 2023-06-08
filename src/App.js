a3b41689e206255681c162eea94533eb5bfbdb6d
jiael <jiael888@gmail.com>
2023-06-08 10:06:12 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactImageGallery from "./ReactImageGallery";
+import Navbar from "./Navbar";
+import {HashRouter as Router,Routes,Route} from 'react-router-dom';
+import About from './About';
+import Featured from './Featured';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/elane_photography' element={<ReactImageGallery/>}/>
+        <Route path='/elane_photography/featured' element={<Featured/>}/>
+        <Route path='/elane_photography/about' element={<About/>}/>
+      </Routes>
+    </Router>
   );
 }
 
