f6f0bac7ed54a12b1ada26e9bef3b5fa627559b6
jiael <jiael888@gmail.com>
2024-01-26 23:36:21 -0500

    added pictures, disable right click

--- src/ReactImageGallery.js
+++ src/ReactImageGallery.js
@@ -1,26 +1,27 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import Masonry, {ResponsiveMasonry} from "react-responsive-masonry";
 import './ReactImageGallery.css';
 import Navbar from './Navbar';
 
 const images = [
-    "../elane_photography/images/024.jpg",//HELPPPPP
-    "../elane_photography/images/04.jpg",
-    "../elane_photography/images/01.jpg",
+
     "../elane_photography/images/03.jpg",
     "../elane_photography/images/030.jpg",
+    "../elane_photography/images/01.jpg",
+    "../elane_photography/images/015.jpg",//HELPPPPP
+    "../elane_photography/images/04.jpg",
     "../elane_photography/images/017.jpg",
-    "../elane_photography/images/015.jpg",
+    "../elane_photography/images/024.jpg",
     "../elane_photography/images/07.jpg",
-    "../elane_photography/images/06.jpg",
+    "../elane_photography/images/011.jpg",
     "../elane_photography/images/010.jpg",
     "../elane_photography/images/014.jpg",
     "../elane_photography/images/08.jpg",
     "../elane_photography/images/012.jpg",
-    "../elane_photography/images/013.jpg",
+    "../elane_photography/images/019.jpg",
     "../elane_photography/images/05.jpg",
     "../elane_photography/images/021.jpg",
-    "../elane_photography/images/019.jpg",
+    "../elane_photography/images/013.jpg",
     "../elane_photography/images/018.jpg",
     "../elane_photography/images/028.jpg",
     "../elane_photography/images/025.jpg",
@@ -31,9 +32,20 @@ const images = [
     "../elane_photography/images/031.jpg",
     "../elane_photography/images/029.jpg",
     "../elane_photography/images/02.jpg",
-    "../elane_photography/images/011.jpg",
+    "../elane_photography/images/06.jpg",
     "../elane_photography/images/032.jpg",
-    "../elane_photography/images/033.jpg"
+    "../elane_photography/images/033.jpg",
+    "../elane_photography/images/034.jpg",
+    "../elane_photography/images/035.jpg",
+    "../elane_photography/images/038.jpg",
+    "../elane_photography/images/040.jpg",
+    "../elane_photography/images/036.jpg",
+    "../elane_photography/images/037.jpg",
+    "../elane_photography/images/039.jpg",
+    "../elane_photography/images/043.jpg",
+    "../elane_photography/images/042.jpg",
+    "../elane_photography/images/041.jpg"
+    
   ]
 
 // const descriptions = [
@@ -75,7 +87,15 @@ const ReactImageGallery = () => {
             setData({img: '', i: 0});
         }
     }
-
+    useEffect(() => {
+        const handleContextmenu = e => {
+            e.preventDefault()
+        }
+        document.addEventListener('contextmenu', handleContextmenu)
+        return function cleanup() {
+            document.removeEventListener('contextmenu', handleContextmenu)
+        }
+}, [ ])
     return(
 
         <>
